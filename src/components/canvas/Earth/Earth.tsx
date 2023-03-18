1814b0e8c8c2ae95727935e17723a0d788061b78
Maciej Zachariasz <47660926+vroeN2@users.noreply.github.com>
2023-03-18 15:50:10 +0100

    Develop (#5)
    * feat: add basic files structure
    
    * feat: change files structure (#1)
    
    * Feat/base page layout (#2)
    
    * fix: file names and imports
    
    * feat: create navbar
    
    * create and style navbar
    * install extra dependencies
    * add logo
    
    * feat: add hero section base
    
    * feat: change background and theme
    
    * create new background image
    * change color theme from purple to blue
    * add hero description
    
    * feat: add personalized 3d PC
    
    * add Public folder with PC and Planet assets
    * create and add PC model to the hero page
    * personalize PC and add interactivity
    * create loader
    
    * feat:  add responsiveness to 3d PC
    
    * add scaling and fix position when on smaller screens
    * add extra 3D model to public folder
    * add responsiveness for hero page
    * add scroll indicator
    
    * feat: add Overview section
    
    * create Section Wrapper component to animate and position all sections
    * fix anchor tag on scroll div
    * update logo in the title
    * update mouse pad texture
    * remove react-tilt
    * add react-parallax-tilt
    
    * feat: add Experience section
    
    * create experience cards
    * change border colors of the service cards
    * add types for react-vertical-timeline-component
    
    * feat: add Tech section
    
    * create 3d balls to display technologies
    * add animations/behaviour
    * add accent color for future usage
    
    * feat: add projects section
    
    * feat: add testimonials section (#3)
    
    * rearrange folders structure - put all cards into separate folders
    * put all main components into views folder
    * change theme colours
    
    * Feat/contact form (#4)
    
    * feat: add contact form layout
    
    * feat: add earth 3d model
    
    * rename ComputerCanvas to ComputersCanvas to match the convention
    
    * feat: add stars to the contact view
    
    * add and animate stars in the background
    * amend imports
    * update maath package
    
    * feat: add contact logic

---
+++ src/components/canvas/Earth/Earth.tsx
@@ -0,0 +1,15 @@
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+import CanvasLoader from "../../Loader";
+
+const Earth = () => {
+  const earth = useGLTF("./planet/scene.gltf");
+
+  return (
+    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+  );
+};
+
+export default Earth;
---
+++ src/components/views/Experience/Experience.tsx
@@ -0,0 +1,32 @@
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import { motion } from "framer-motion";
+import "react-vertical-timeline-component/style.min.css";
+
+import { styles } from "../../../styles";
+import { experiences } from "../../../constants";
+import { textVariant } from "../../../../utills/motion";
+import ExperienceCard from "../../Cards/ExperienceCard";
+
+const Experience = () => {
+  return (
+    <>
+      <motion.div variants={textVariant(0)}>
+        <p className={styles.sectionSubText}>What I have done so far</p>
+        <h2 className={styles.sectionHeadText}>Experience.</h2>
+      </motion.div>
+
+      <div className="mt-20 flex flex-col">
+        <VerticalTimeline>
+          {experiences.map((experience, index) => (
+            <ExperienceCard key={index} experience={experience} />
+          ))}
+        </VerticalTimeline>
+      </div>
+    </>
+  );
+};
+
+export default Experience;
