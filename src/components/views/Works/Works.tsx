6f5c9592fb85ce911d78cbe4f7bd58b47842f32c
Maciej Zachariasz <47660926+vroeN2@users.noreply.github.com>
2023-03-18 19:27:58 +0100

    feat: replace mock data
    * add proper icons and file previews
    * remove redundant files

--- src/components/views/Works/Works.tsx
+++ src/components/views/Works/Works.tsx
@@ -24,7 +24,7 @@ const Works = () => {
           projects effectively.
         </motion.p>
       </div>
-      div{" "}
+
       <div className="mt-20 flex flex-wrap gap-7">
         {projects.map((project, index) => (
           <ProjectCard key={`project-${index}`} index={index} {...project} />
